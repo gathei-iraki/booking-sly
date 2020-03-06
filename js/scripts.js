aab8c42c7f942297f06790a996243d44ba0f332c
FaithKavesu <60645017+FaithKavesu@users.noreply.github.com>
2020-03-06 09:08:22 +0300

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+function getTotalAmount() {
+  prompt("Enter your name");
+  
+  prompt("enter your business no");
+    prompt("enter your business no");
+    prompt("enter your account no.");
+    prompt("enter your mpesa pin");
+    alert("Thank you for booking with us");
+    alert("will send you your ticket number to your email address. ");
+    alert("");
+  };
+$("form#contactform").submit(function(event){
+    event.preventDefault();
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var mess = $("#message").val();
+    alert("Hello " + name + " ,Thank You for booking with Us. Have a safe journey");
+    });
